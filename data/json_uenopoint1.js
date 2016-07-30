c89ccd3b716da51c53f854aeca2e850769975d64
sagara1020 <sagara1020@gmail.com>
2016-07-30 11:27:39 +0900

    map

---
+++ data/json_uenopoint1.js
@@ -0,0 +1,16 @@
+var json_uenopoint1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "根津駅", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.766499996185303, 35.716953092366822 ] } },
+{ "type": "Feature", "properties": { "Name": "弥生二丁目遺跡", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.763624668121309, 35.715785828821829 ] } },
+{ "type": "Feature", "properties": { "Name": "不忍池", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.769117832183838, 35.71043710406822 ] } },
+{ "type": "Feature", "properties": { "Name": "湯島天神", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.767422676086426, 35.707631929032402 ] } },
+{ "type": "Feature", "properties": { "Name": "西郷隆盛像", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.773988723754883, 35.71183094314749 ] } },
+{ "type": "Feature", "properties": { "Name": "野口英世像", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.775383472442627, 35.716073290833975 ] } },
+{ "type": "Feature", "properties": { "Name": "上野動物園", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.772910475730896, 35.715855516677557 ] } },
+{ "type": "Feature", "properties": { "Name": "上野御徒町", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.770705699920654, 35.708537959092077 ] } },
+{ "type": "Feature", "properties": { "Name": "竹の台広場", "descriptio": null }, "geometry": { "type": "Point", "coordinates": [ 139.774207779965707, 35.716233277612893 ] } }
+]
+}
